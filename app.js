ebdcf0d38b87ee8dc6a77df9d6463d0565dd16f1
AlexisCadena <alebis5513@gmail.com>
2025-08-03 16:27:00 -0600

    Se declara una variable llamada nombreAmigo que a través del document.getElementById obtenemos los nombres que se han ingresado

--- app.js
+++ app.js
@@ -2,3 +2,9 @@
 
 //Declaramos una variable de tipo array que almacenará los nombres de los amigos ingresados
 let amigos = [];
+//Creamos una función para agregar amigos al array
+function agregarAmigo(nombre){
+    //1.- Capturamos el valor del la entrada del cuadro de texto
+    let nombreAmigo = document.getElementById("nombre");
+    
+}
