ee6d731f29955b30ca44f01ebe50683f5c5c2dff
AlexisCadena <alebis5513@gmail.com>
2025-08-03 17:04:23 -0600

    Se corrgien algunos errores en la función agregarAmigo

--- app.js
+++ app.js
@@ -3,13 +3,18 @@
 //Declaramos una variable de tipo array que almacenará los nombres de los amigos ingresados
 let amigos = [];
 //Creamos una función para agregar amigos al array
-function agregarAmigo(nombre){
+function agregarAmigo(){
     //1.- Capturamos el valor del la entrada del cuadro de texto
-    let nombreAmigo = document.getElementById("nombre");
-
+    let nombreAmigo = document.getElementById("amigo");
+    
     //2.- Validamos que el cuadro de texto donde se ingresará el nombre no se encuentre vacío
-    if(nombreAmigo.value.trim() === ""){
+    if(nombreAmigo.value === ""){
         alert("Por favor, inserte un nombre");
         return;
     }
+
+    //3.- Agregamos el nombre del amigo al array
+    amigos.push(nombreAmigo.value);
+
+    console.log(amigos);
 }
